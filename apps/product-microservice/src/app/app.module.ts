5ef4c8590ffd4dc437460ccf197793a980dcd5f2
Uzeyr OZCAN <uzeyr@oktein.com>
2022-12-31 13:55:18 +0300

    Product micro service module changed/ fixed

--- apps/product-microservice/src/app/app.module.ts
+++ apps/product-microservice/src/app/app.module.ts
@@ -2,10 +2,42 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ServiceName } from '@nest-microservice-kafka/shared/enum';
+import { ProductRepository } from './product.repository';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: ServiceName.USER_MICROSERVICE,
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: ServiceName.USER,
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: ServiceName.USER_CONSUMER,
+          },
+        },
+      },
+      {
+        name: ServiceName.PRODUCT_MICROSERVICE,
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: ServiceName.PRODUCT,
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: ServiceName.PRODUCT_CONSUMER,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ProductRepository],
 })
 export class AppModule {}
--- apps/product-microservice/src/main.ts
+++ apps/product-microservice/src/main.ts
@@ -3,20 +3,29 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { ServiceName } from '@nest-microservice-kafka/shared/enum';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+
+      options: {
+        client: {
+          brokers: ['localhost:9092'],
+        },
+        consumer: {
+          groupId: ServiceName.PRODUCT_CONSUMER,
+        },
+      },
+    }
   );
+  await app.listen();
 }
 
 bootstrap();
